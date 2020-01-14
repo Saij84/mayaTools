ee1af1ee1e1e3581d31057dd7046c936ca8a39cc
Fang <saij84@gmail.com>
2020-01-14 18:35:03 +1100

    connection tool update

--- connection_utils/connectByType.py
+++ connection_utils/connectByType.py
@@ -81,17 +81,15 @@ def connectNodes(srcMobjHandle, trgMobjHandle):
     srcPlug = srcMFn.findPlug(srcOutputPlugs, False)
     trgPlug = trgMFn.findPlug(trgInputPlugs, False)
 
-    if srcMFn.hasAttribute("worldMatrix") and not trgInputPlugs == "matrixIn":
+    if srcPlug.isArray and not trgMFn.hasAttribute("matrixIn"):
         srcWorldMtxPlug = srcPlug.elementByLogicalIndex(0)
         dgMod.connect(srcWorldMtxPlug, trgPlug)
         print("connecting: {} to {}".format(srcWorldMtxPlug, trgPlug))
-
     elif trgMFn.hasAttribute("matrixIn"):
         srcWorldMtxPlug = srcPlug.elementByLogicalIndex(0)
         trgMtxPlug = trgPlug.elementByLogicalIndex(0)
         dgMod.connect(srcWorldMtxPlug, trgMtxPlug)
         print("connecting: {} to {}".format(srcWorldMtxPlug, trgMtxPlug))
-
     else:
         dgMod.connect(srcPlug, trgPlug)
 
