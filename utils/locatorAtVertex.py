44f58fe2b765603505961be919086e03a0221968
Fang <saij84@gmail.com>
2020-02-16 21:26:28 +1100

    added check for selection

--- utils/locatorAtVertex.py
+++ utils/locatorAtVertex.py
@@ -1,42 +1,68 @@
+"""
+Author:Fangmin Chen
+Version: 0.1
+
+This script will add an locator at a selected vertex aligned to its normals
+
+USAGE: Select mesh vertex, run script
+
+ToDo: Add functionality to put a locator at the face center aligned to its normal
+"""
 import maya.api.OpenMaya as om2
 
 selList = om2.MGlobal.getActiveSelectionList()
-mObjs = [selList.getDependNode(idx) for idx in range(selList.length())]
+
 
 def getVtxIDs(selList):
+    """
+    Get selected vertexID/s
+    :param selList: MSelectionList
+    :return: list of int
+    """
     __, vtxs = selList.getComponent(0)
     vtxList = om2.MFnSingleIndexedComponent(vtxs)
     vtxIdxList = vtxList.getElements()
-    return vtxIdxList
+    selType = vtxs.apiType()
+
+    return vtxIdxList, selType
+
 
 def createLocator(vtxID):
+    """
+    create a locator with vertexID in the name
+    :param vtxID: int
+    :return: MObjectHandle
+    """
     mDagMod = om2.MDagModifier()
     loc = mDagMod.createNode("locator")
-    mDagMod.renameNode(loc, "{}_LOC".format(vtxID))
+    newName = "LOC_{}".format(vtxID)
+    mDagMod.renameNode(loc, newName)
     mDagMod.doIt()
 
     locMObjHandle = om2.MObjectHandle(loc)
     return locMObjHandle
 
-vtxIDs = getVtxIDs(selList)
-
-for vtxID in vtxIDs:
-    loc = createLocator(vtxID)
-    mFnMesh = om2.MFnMesh(mObjs[0])
+def createLocAtVertex():
+    # Get vertex normal/position
+    meshMObj = selList.getDependNode(0)
+    mFnMesh = om2.MFnMesh(meshMObj)
     vtxNormal = mFnMesh.getVertexNormal(vtxID, False, om2.MSpace.kObject)
     vtxPoint = mFnMesh.getPoint(vtxID)
 
+    # Construct a matrix
     mtxConstruct = (
         vtxNormal[0], vtxNormal[1], vtxNormal[2], 0,
         0, 1, 0, 0,
         0, 0, 1, 0,
         vtxPoint[0], vtxPoint[1], vtxPoint[2], vtxPoint[3]
     )
-    vtxMMatrix = om2.MMatrix(mtxConstruct)
+
+    vtxMMatrix = om2.MMatrix(mtxConstruct)  # Convert to Maya MMatrix
     vtxMtransMtx = om2.MTransformationMatrix(vtxMMatrix)
 
+    # Get rotation/translation
     rot = vtxMtransMtx.rotation(asQuaternion=False)
-    trans =vtxMtransMtx.translation(om2.MSpace.kWorld)
+    trans = vtxMtransMtx.translation(om2.MSpace.kWorld)
 
     if loc.isValid():
         locMObj = loc.object()
@@ -57,3 +83,14 @@ for vtxID in vtxIDs:
         rotX.setFloat(rot[0])
         rotY.setFloat(rot[1])
         rotZ.setFloat(rot[2])
+
+vtxIDs, typeID = getVtxIDs(selList)
+
+for vtxID in vtxIDs:
+    loc = createLocator(vtxID)
+    if typeID == 550:  #  kMeshVertComponent
+        createLocAtVertex()
+    elif typeID == 548:  # kMeshPolygonComponent
+        pass
+    else:
+        pass
