e001475d041e6b1f8a3a5fc7ee82f31094891c6a
Fang <saij84@gmail.com>
2020-03-09 07:31:20 +1100

    add matrix2vector script

--- tests/test2.py
+++ tests/test2.py
@@ -1,110 +1,71 @@
-"""
-Author:Fangmin Chen
-Version: 0.1
+# encoding: utf8
 
-This script will add an locator at a selected vertex aligned to its normals
-
-USAGE: Select mesh vertex, run script
-
-ToDo: Add functionality to put a locator at the face center aligned to its normal
-"""
-import collections as coll
 import maya.api.OpenMaya as om2
+import math
 
 
-def geIDs(selList):
-    """
-    Get selected component's ID/s
-    :param selList: MSelectionList
-    :return: list of int
-    """
-    __, id = selList.getComponent(0)
-    idList = om2.MFnSingleIndexedComponent(id)
-    idElement = idList.getElements()
-    selType = id.apiType()
-
-    return idElement, selType
-
-
-def createLocator(componentID):
-    """
-    create a locator with vertexID in the name
-    :param componentID: int
-    :return: MObjectHandle
-    """
-    mDagMod = om2.MDagModifier()
-    loc = mDagMod.createNode("locator")
-    newName = "LOC_{}".format(componentID)
-    mDagMod.renameNode(loc, newName)
-    mDagMod.doIt()
-
-    locMObjHandle = om2.MObjectHandle(loc)
-    return locMObjHandle
-
-
-def createLocAtVertex(componentID, selList):
-    # Get vertex normal/position
-    meshDagPath = selList.getDagPath(0)
-    mObj = selList.getDependNode(0)
-    mFn = om2.MFnDependencyNode(mObj)
-    wMtxTopPlug = mFn.findPlug("worldMatrix", False)
-    wMtxPlug = wMtxTopPlug.elementByLogicalIndex(0)
-    wMtxMObj = wMtxPlug.asMObject()
-
-    print(om2.MFnMatrixArrayData(wMtxMObj))
-
-
-    mFnMesh = om2.MFnMesh(meshDagPath)
-    vtxNormal = mFnMesh.getVertexNormal(componentID, False, om2.MSpace.kWorld)
-    vtxPoint = mFnMesh.getPoint(componentID, om2.MSpace.kWorld)
-
-    # Construct a matrix
-    mtxConstruct = (
-        vtxNormal[0], vtxNormal[1], vtxNormal[2], 0,
-        0, 1, 0, 0,
-        0, 0, 1, 0,
-        vtxPoint[0], vtxPoint[1], vtxPoint[2], vtxPoint[3]
-    )
-
-    vtxMMatrix = om2.MMatrix(mtxConstruct)  # Convert to Maya MMatrix
-    vtxMtransMtx = om2.MTransformationMatrix(vtxMMatrix)
-
-    # # Get rotation/translation
-    # rot = vtxMtransMtx.rotation(asQuaternion=False)
-    # trans = vtxMtransMtx.translation(om2.MSpace.kWorld)
-    # axisData = coll.namedtuple("axis", ["X", "Y", "Z"])
-    # translate = axisData(trans[0], trans[1], trans[2])
-    # rotate = axisData(rot[0], rot[1], rot[2])
-    #
-    # if loc.isValid():
-    #     locMObj = loc.object()
-    #     mFn = om2.MFnDependencyNode(locMObj)
-    #
-    #     transX = mFn.findPlug("translateX", False)
-    #     transY = mFn.findPlug("translateY", False)
-    #     transZ = mFn.findPlug("translateZ", False)
-    #
-    #     transX.setFloat(translate.X)
-    #     transY.setFloat(translate.Y)
-    #     transZ.setFloat(translate.Z)
-    #
-    #     rotX = mFn.findPlug("rotateX", False)
-    #     rotY = mFn.findPlug("rotateY", False)
-    #     rotZ = mFn.findPlug("rotateZ", False)
-    #
-    #     rotX.setFloat(rotate.X)
-    #     rotY.setFloat(rotate.Y)
-    #     rotZ.setFloat(rotate.Z)
-
-
-selList = om2.MGlobal.getActiveSelectionList()
-componentIDs, typeID = geIDs(selList)
-
-if typeID == 550:  # kMeshVertComponent
-    for componentID in componentIDs:
-        # loc = createLocator(componentID)
-        createLocAtVertex(componentID, selList)
-elif typeID == 548:  # kMeshPolygonComponent
+def maya_useNewAPI():
     pass
-else:
-    pass
+
+
+class SimpleNode(om2.MPxNode):
+    node_name = 'simpleNode'
+    node_id = om2.MTypeId(0x84002)
+    a_arg = om2.MMatrix()
+    a_amp = None
+    a_val = None
+
+    def __init__(self):
+        om2.MPxNode.__init__(self)
+
+    @staticmethod
+    def creator():
+        return SimpleNode()
+
+    @staticmethod
+    def initialize():
+        num_attr = om2.MFnNumericAttribute()
+        mtx_attr = om2.MFnMatrixAttribute()
+
+        SimpleNode.a_arg = mtx_attr.create('matrixIn', 'mtxIn', om2.MFnNumericData.kFloat)
+        mtx_attr.writable = True
+        mtx_attr.storable = True
+        om2.MPxNode.addAttribute(SimpleNode.a_arg)
+
+        SimpleNode.a_amp = num_attr.create('amp', 'amp', om2.MFnNumericData.kFloat, 1.0)
+        num_attr.storable = True
+        om2.MPxNode.addAttribute(SimpleNode.a_amp)
+
+        SimpleNode.a_val = num_attr.create('val', 'val', om2.MFnNumericData.kFloat, 0.0)
+        num_attr.storable = True
+        num_attr.writable = True
+        om2.MPxNode.addAttribute(SimpleNode.a_val)
+
+        om2.MPxNode.attributeAffects(SimpleNode.a_arg, SimpleNode.a_val)
+        om2.MPxNode.attributeAffects(SimpleNode.a_amp, SimpleNode.a_val)
+
+    def compute(self, plug, data):
+        # type: (om.MPlug, om.MDataBlock) -> None
+
+        state = om2.MFnDependencyNode(self.thisMObject()).findPlug('nodeState', False).asInt()
+        if state == 1:
+            data.outputValue(SimpleNode.a_val).setFloat(data.inputValue(SimpleNode.a_arg).asFloat())
+            return
+
+        if plug == SimpleNode.a_val:
+            arg = data.inputValue(SimpleNode.a_arg).asFloat()
+            amp = data.inputValue(SimpleNode.a_amp).asFloat()
+
+            val_handle = data.outputValue(SimpleNode.a_val)  # type: om.MDataHandle
+            val_handle.setFloat(amp * math.sin(arg))
+            data.setClean(plug)
+
+
+def initializePlugin(obj):
+    fn_plugin = om2.MFnPlugin(obj, 'ilya radovilsky', '1.0')
+    fn_plugin.registerNode(SimpleNode.node_name, SimpleNode.node_id, SimpleNode.creator, SimpleNode.initialize)
+
+
+def uninitializePlugin(obj):
+    fn_plugin = om2.MFnPlugin(obj)
+    fn_plugin.deregisterNode(SimpleNode.node_id)
--- utils/matrixVisualizer.py
+++ utils/matrixVisualizer.py
@@ -1,128 +1,75 @@
+import sys
 import maya.api.OpenMaya as om2
+import maya.OpenMayaMPx as OpenMayaMPx
 
-def getNodeMatrix(mObjHandle, searchMatrix="worldMatrix"):
-    """
-    Search for a matrix plug and return it as a MMatrix
-    :param mObjHandle: MObjectHandle
-    :param searchMatrix: string
-    :return: MMatrix
-    """
-    if mObjHandle.isValid():
-        mObj = mObjHandle.object()
-        mFn = om2.MFnDependencyNode(mObj)
-        getMtxPlug = mFn.findPlug(searchMatrix, False)
+# ... additional imports here ...
 
-        mtxPlug = getMtxPlug
-        if getMtxPlug.isArray:
-            mtxPlug = getMtxPlug.elementByLogicalIndex(0)
-        plugMObj = mtxPlug.asMObject()
-
-        mFnMtxData = om2.MFnMatrixData(plugMObj)
-        mMatrixData = mFnMtxData.matrix()
-
-        return mMatrixData
-
-def createLocator(name, mDagMod):
-    """
-    create a locator with vertexID in the name
-    :param componentID: int
-    :return: MObjectHandle
-    """
-    locLocalScale = 0.1
-    mDagMod = om2.MDagModifier()
-    mDagPath = om2.MDagPath()
-    loc = mDagMod.createNode("locator")
-    newName = "LOC_f{}".format(name)
-    mDagMod.renameNode(loc, newName)
-
-    locMObjHandle = om2.MObjectHandle(loc)
-    mDagMod.doIt()
-
-    dagPath = mDagPath.getAPathTo(loc)
-    shapeDagPath = dagPath.extendToShape()
-    shapeMObj = shapeDagPath.node()
-    shapeMFn = om2.MFnDependencyNode(shapeMObj)
-
-    shapeLocalScaleX = shapeMFn.findPlug("localScaleX", False)
-    shapeLocalScaleY = shapeMFn.findPlug("localScaleY", False)
-    shapeLocalScaleZ = shapeMFn.findPlug("localScaleZ", False)
-    shapeLocalScaleX.setFloat(locLocalScale)
-    shapeLocalScaleY.setFloat(locLocalScale)
-    shapeLocalScaleZ.setFloat(locLocalScale)
-
-    return locMObjHandle
+kPluginCmdName = 'mtx2Vectors'
 
 
-def getPolygonCenter(selList, mDagMod):
-    """
-    Method to iter through an selection list components and return center of an polygon
-    :param selList: MSelectionList
-    :return: list
-    """
-    iter = om2.MItSelectionList(selList, om2.MFn.kMeshPolygonComponent)
-    mObj = selList.getDependNode(0)
-    mObjHandle = om2.MObjectHandle(mObj)
+##########################################################
+# Plug-in
+##########################################################
+class Mtx2Vector(OpenMayaMPx.MPxCommand):
 
-    offsetMtx = getNodeMatrix(mObjHandle)
+    def __init__(self):
+        ''' Constructor. '''
+        OpenMayaMPx.MPxCommand.__init__(self)
 
-    while not iter.isDone():
-        dag, mObj = selList.getComponent(0)
-        idList = om2.MFnSingleIndexedComponent(mObj)
-        idElement = idList.getElements()
-        polyIter = om2.MItMeshPolygon(dag, mObj)
-        while not polyIter.isDone():
-            triMPointList, triVtxID = polyIter.getTriangle(0)
-            point1 = triMPointList[0]
-            point2 = triMPointList[1]
-            point3 = triMPointList[2]
+    def doIt(self, args):
+        ''' Command execution. '''
 
-            polygonCenterMPoint = polyIter.center(om2.MSpace.kObject)
+        testMtxInput = [
+            1, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1
+        ]
 
-            p1Vector = om2.MVector(point1.x, point1.y, point1.z)
-            p2Vector = om2.MVector(point2.x, point2.y, point2.z)
-            p3Vector = om2.MVector(point3.x, point3.y, point3.z)
+        mtxInput = om2.MMatrix(testMtxInput)
+        mTransMtx = om2.MTransformationMatrix(mtxInput)
 
-            p1MidVector = p2Vector - p1Vector
-            p2MidVector = p3Vector - p1Vector
+        mtxRow1 = [mtxInput[idx] for idx in range(0, 4)]
+        mtxRow2 = [mtxInput[idx] for idx in range(4, 8)]
+        mtxRow3 = [mtxInput[idx] for idx in range(8, 12)]
+        mtxRow4 = [mtxInput[idx] for idx in range(12, 16)]
 
-            vector1 = point3 - point1
-            vector2 = point2 - point1
-            normalVector = vector1 ^ -vector2
 
-            mtx = (
-                normalVector.x, normalVector.y, normalVector.z, 0,
-                p2MidVector.x, p2MidVector.y, p2MidVector.z, 0,
-                p1MidVector.x, p1MidVector.y, p1MidVector.z, 0,
-                polygonCenterMPoint.x, polygonCenterMPoint.y, polygonCenterMPoint.z, polygonCenterMPoint.w
-            )
+##########################################################
+# Plug-in initialization.
+##########################################################
+def cmdCreator():
+    ''' Create an instance of our command. '''
+    return OpenMayaMPx.asMPxPtr(Mtx2Vector())
 
-            compositMtx = om2.MMatrix(mtx) * offsetMtx
-            mTransMtx = om2.MTransformationMatrix(compositMtx)
-            trans = mTransMtx.translation(om2.MSpace.kWorld)
-            rot = mTransMtx.rotation()
 
-            locMObjHandle = createLocator(idElement[0], mDagMod)
-            if locMObjHandle.isValid():
-                locMObj = locMObjHandle.object()
-                locMFn = om2.MFnDependencyNode(locMObj)
+def initializePlugin(mobject):
+    ''' Initialize the plug-in when Maya loads it. '''
+    mplugin = OpenMayaMPx.MFnPlugin(mobject)
+    try:
+        mplugin.registerCommand(kPluginCmdName, cmdCreator)
+    except:
+        sys.stderr.write('Failed to register command: ' + kPluginCmdName)
 
-                transX = locMFn.findPlug("translateX", False)
-                transY = locMFn.findPlug("translateY", False)
-                transZ = locMFn.findPlug("translateZ", False)
-                transX.setFloat(trans.x)
-                transY.setFloat(trans.y)
-                transZ.setFloat(trans.z)
 
-                rotX = locMFn.findPlug("rotateX", False)
-                rotY = locMFn.findPlug("rotateY", False)
-                rotZ = locMFn.findPlug("rotateZ", False)
-                rotX.setFloat(rot.x)
-                rotY.setFloat(rot.y)
-                rotZ.setFloat(rot.z)
-            polyIter.next(0)
-        iter.next()
+def uninitializePlugin(mobject):
+    ''' Uninitialize the plug-in when Maya un-loads it. '''
+    mplugin = OpenMayaMPx.MFnPlugin(mobject)
+    try:
+        mplugin.deregisterCommand(kPluginCmdName)
+    except:
+        sys.stderr.write('Failed to unregister command: ' + kPluginCmdName)
 
-selList = om2.MGlobal.getActiveSelectionList()
-mDagMod = om2.MDagModifier()
 
-getPolygonCenter(selList, mDagMod)
+##########################################################
+# Sample usage.
+##########################################################
+''' 
+# Copy the following lines and run them in Maya's Python Script Editor:
+
+import maya.cmds as cmds
+cmds.loadPlugin('Mtx2Vector.py')
+cmds.Mtx2Vector()
+
+'''
+
