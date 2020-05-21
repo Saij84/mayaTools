bdce7fac5320e3e1c6ec37e478359b28cd227bbb
Fang Chen <saij84@gmail.com>
2020-05-21 21:35:23 +1000

    new script added: createLocatorAtSelection

---
+++ utils/createLocatorAtSelection.py
@@ -0,0 +1,65 @@
+import maya.api.OpenMaya as om2
+
+
+def createNode(nodeTypeName, nodeName, mDagMod):
+    """
+    Creates and names the created node
+    :param nodeTypeName: str
+    :param nodeName: str
+    :param mDagMod: MDagModifier
+    :return: MObjectHandle
+    """
+    nodeMObj = mDagMod.createNode(nodeTypeName)
+    mDagMod.renameNode(nodeMObj, nodeName)
+    nodeMObjHandle = om2.MObjectHandle(nodeMObj)
+    mDagMod.doIt()
+
+    return nodeMObjHandle
+
+
+def setAtters(mObjectHandle, mtx):
+    """
+    Sets translation/rotation
+    :param mObjectHandle: MObjectHandle
+    :param mtx: MMatrix
+    :return: None
+    """
+    if mObjectHandle.isValid():
+        mObj = mObjectHandle.object()
+        mFn = om2.MFnDependencyNode(mObj)
+        mTransMtx = om2.MTransformationMatrix(mtx)
+
+        trans = mTransMtx.translation(om2.MSpace.kWorld)
+        rot = mTransMtx.rotation()
+
+        transX = mFn.findPlug("translateX", False)
+        transY = mFn.findPlug("translateY", False)
+        transZ = mFn.findPlug("translateZ", False)
+        transX.setFloat(trans.x)
+        transY.setFloat(trans.y)
+        transZ.setFloat(trans.z)
+
+        rotX = mFn.findPlug("rotateX", False)
+        rotY = mFn.findPlug("rotateY", False)
+        rotZ = mFn.findPlug("rotateZ", False)
+        rotX.setFloat(rot.x)
+        rotY.setFloat(rot.y)
+        rotZ.setFloat(rot.z)
+
+
+def findPlug(mObjectHandle, findPlug):
+    """
+    Creates and names the created node
+    :param mObjectHandle: MObjectHandle
+    :param findPlug: str
+    :return: MPlug
+    """
+    pass
+
+selList = om2.MGlobal.getActiveSelectionList()
+mObjs = [selList.getDependNode(idx) for idx in range(selList.length())]
+mDagMod = om2.MDagModifier()
+
+for mObj in mObjs:
+    mFn = om2.MFnDependencyNode(mObj)
+    createNode("locator", "TEST", mDagMod)
